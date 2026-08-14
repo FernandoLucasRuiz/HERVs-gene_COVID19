28af7e8018d3865b1d60d3a4366678932938ab94
Fernando Lucas-Ruiz <fernando.lucasrz@gmail.com>
2026-08-14 12:29:34 +0200

    Delete code/packages.R

--- code/packages.R
+++
@@ -1,9 +0,0 @@
-pkgs <- c(
-    "tidyverse", "knitr", "readxl", "DESeq2", "gtsummary", "tximport",
-    "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db", "circlize",
-    "tidyheatmaps", "viridisLite", "clusterProfiler", "ReactomePA",
-    "DOSE", "biomaRt", "GWENA", "ggsankey", "ggpubr", "ggrepel",
-    "igraph", "scales"
-)
-
-invisible(lapply(pkgs, library, character.only = TRUE))
